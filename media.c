caca370f883685f551006a245cd77bd21d09b3e7
WadsonBrito <wadsonmelo@gmail.com>
2025-03-05 14:13:23 +0000

    calculo da media

--- media.c
+++ media.c
@@ -1,1 +1,19 @@
+# include <stdio.h>
+int main () {
+float nota1, nota2, nota3, media;
 
+printf("***** Calculo da média *****\n");
+
+printf(" Infome sua primera nota: \n");
+scanf("%f", &nota1);
+printf(" Infome sua segunda nota: \n");
+scanf("%f", &nota2);
+printf(" Infome sua terceira nota: \n");
+scanf("%f", &nota3);
+
+media = (float)(nota1 + nota2 + nota3) / 3;
+
+printf("Sua média é: %.1f\n", media);
+
+    return 0;
+}
